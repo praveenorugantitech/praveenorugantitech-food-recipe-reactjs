68f5e6af15d9a7cee71d24b7402e5f92e7a6217b
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-02-14 01:24:25 +0530

    Food Recipe

---
+++ README.md
@@ -0,0 +1,32 @@
+# Food Receipe Developed using React JS amd Edamam Receipe API
+ 
+### API
+
+[https://api.edamam.com/search?q=${search}&app_id=${process.env.REACT_APP_CLIENTID}&app_key=${process.env.REACT_APP_CLIENTSECRET}](https://api.edamam.com/search?q=${search}&app_id=${process.env.REACT_APP_CLIENTID}&app_key=${process.env.REACT_APP_CLIENTSECRET})
+
+### App Screenshots
+
+![screenshot of the app](https://raw.githubusercontent.com/praveenorugantitech/praveenorugantitech-food-recipe-reactjs/master/src/images/screenshot.PNG)
+
+
+### Live Demo
+
+[https://praveenorugantitech.github.io/praveenorugantitech-food-recipe-reactjs/](https://praveenorugantitech.github.io/praveenorugantitech-food-recipe-reactjs/)
+
+
+### Setup Local Instructions
+
+```
+git clone https://github.com/praveenorugantitech/praveenorugantitech-food-recipe-reactjs.git
+cd praveenorugantitech-food-recipe-reactjs
+
+create .env file and include your API Key related to Edamam Receipe API
+
+REACT_APP_CLIENTID=""
+REACT_APP_CLIENTSECRET=""
+
+
+npm install
+npm start
+
+```
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from "axios";
+
+function App() {
+  const [search, setSerach] = useState("Dosa");
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${process.env.REACT_APP_CLIENTID}&app_key=${process.env.REACT_APP_CLIENTSECRET}`
+    );
+    setRecipes(res.data.hits);
+  };
+
+  const onInputChange = e => {
+    setSerach(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    getRecipes();
+  };
+  return (
+    <div className="App">
+      <Header
+        search={search}
+        onInputChange={onInputChange}
+        onSearchClick={onSearchClick}
+      />
+      <div className="container">
+        <Recipes recipes={recipes} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
